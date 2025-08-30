faa1e1389d91e778b3677970b79ce16f3892a139
GTUNE\ktc <ktc23a31a0021@edu.kyoto-tech.ac.jp>
2025-08-30 13:07:50 +0900

    ブラウザに表示させる内容を変更

--- DockerAssignment/htdocs/get_project_1.php
+++ DockerAssignment/htdocs/get_project_1.php
@@ -18,8 +18,10 @@ if ($mysql->connect_error) {
 }
 
 $sql = "SELECT * FROM jobs";
+$sql1 = "SELECT * FROM creaters";
 // クエリの実行
 $result = $mysql->query($sql);
+$result1 = $mysql->query($sql1);
 
 // 結果の処理
 if ($result) {
@@ -28,6 +30,23 @@ if ($result) {
         while ($row = $result->fetch_assoc()) {
             // 各行のデータを表示
             echo "id: " . $row["id"] . ", name: " . $row["name"] . "<br>";
+        }
+    } else {
+        echo "該当するデータはありません。";
+    }
+    // 結果セットを解放
+    // $result->free();
+} else {
+    echo "クエリの実行に失敗しました: " . $mysql->error;
+}
+
+// 結果の処理
+if ($result1) {
+    if ($result1->num_rows > 0) {
+        // 結果セットから行を取得して処理
+        while ($row = $result1->fetch_assoc()) {
+            // 各行のデータを表示
+            echo "id: " . $row["id"] . ", name: " . $row["name"] .  ", progress: " . $row["progress"] .", sub_id: " . $row["sub_id"] ."<br>";
         }
     } else {
         echo "該当するデータはありません。";
